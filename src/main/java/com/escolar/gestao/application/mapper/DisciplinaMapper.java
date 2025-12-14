10f7d0f351cb7474ed4047d34e70f71e0731b2f7
Francisco <galvesa9@gmail.com>
2025-12-14 18:35:40 -0300

    Continuação da implementação

--- src/main/java/com/escolar/gestao/application/mapper/DisciplinaMapper.java
+++ src/main/java/com/escolar/gestao/application/mapper/DisciplinaMapper.java
@@ -4,5 +4,13 @@ import com.escolar.gestao.domain.Disciplina;
 import com.escolar.gestao.infrastructure.persistence.entity.DisciplinaEntity;
 
 public class DisciplinaMapper {
-    public static Disciplina toDomain(DisciplinaEntity)
+    public static Disciplina toDomain(DisciplinaEntity entity){
+        return new Disciplina(
+                entity.nome,
+                entity.cargaHoraria,
+                entity.descricao,
+                entity.codigo,
+                entity.dataCriacao
+        );
+    }
 }
