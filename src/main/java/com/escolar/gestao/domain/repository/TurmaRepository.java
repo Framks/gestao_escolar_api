9115ad0f803133257d0ace5801098af6d88be932
Francisco <galvesa9@gmail.com>
2025-12-14 18:40:13 -0300

    Continuação da implementação

--- src/main/java/com/escolar/gestao/domain/repository/TurmaRepository.java
+++ src/main/java/com/escolar/gestao/domain/repository/TurmaRepository.java
@@ -1,9 +1,11 @@
 package com.escolar.gestao.domain.repository;
 
 import com.escolar.gestao.domain.Turma;
+import java.util.List;
 
 public interface TurmaRepository {
     Turma findByCodigo(String codigo);
     Turma save(Turma turma);
     void deleteByCodigo(String codigo);
+    List<Turma> findAll(Integer page, Integer size, String sort);
 }
--- src/main/java/com/escolar/gestao/infrastructure/persistence/TurmaRepositoryImpl.java
+++ src/main/java/com/escolar/gestao/infrastructure/persistence/TurmaRepositoryImpl.java
@@ -4,7 +4,13 @@ import com.escolar.gestao.application.mapper.TurmaMapper;
 import com.escolar.gestao.domain.Turma;
 import com.escolar.gestao.domain.repository.TurmaRepository;
 import com.escolar.gestao.infrastructure.exception.TurmaNotFounException;
+import com.escolar.gestao.infrastructure.persistence.entity.TurmaEntity;
 import com.escolar.gestao.infrastructure.persistence.jpaRepository.TurmaRepositoryJpa;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -36,5 +42,13 @@ public class TurmaRepositoryImpl implements TurmaRepository {
     @Override
     public void deleteByCodigo(String codigo) {
 
+    }
+
+    @Override
+    public List<Turma> findAll(Integer page, Integer size, String sort) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sort));
+        Page<TurmaEntity> turmas = turmaRepositoryJpa.findAll(pageable);
+
+        return turmas.stream().map(TurmaMapper::toDomain).toList();
     }
 }
